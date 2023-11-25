14a2fa631192c8b0d7b6996fdbcf408ff9def9f5
nhnishat <nhnishat1998@gmail.com>
2023-11-26 01:27:04 +0600

    mongoose added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import mongoose from 'mongoose'
 import app from './app'
-const port = 5000
-app.listen(port, () => {
-  console.log(`mongoose is listening on port ${port}`)
-})
+import config from './app/config'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    app.listen(config.port, () => {
+      console.log(`mongoose is listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+main()
